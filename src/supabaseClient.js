ad5ab16c7136051cdc0401b37981e100fe2cc495
Leandro <zegarralenny10@gmail.com>
2026-07-14 14:55:12 -0300

    Use env vars for Supabase in Vercel

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://dchliszajubvvuupkqlz.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjaGxpc3phanVidnZ1dXBrcWx6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwNTQxNzgsImV4cCI6MjA5NjYzMDE3OH0.Cirv9G7k6suAYU3arkvvoqEMiPEJ8WWbCbJ1n2ffJ6Q';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
